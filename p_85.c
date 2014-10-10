7cc815c3752e38154c4bed8e2456fdec5d8fa65f
Favian Contreras <fnc4@cornell.edu>
2014-10-10 05:35:39 -0700

    Fix abs functions.

--- p_85.c
+++ p_85.c
@@ -7,13 +7,22 @@ int main() {
     int area;
     int sum = 0;
     int closest_diff = 5000000;
-    for (int n = 1, k = 2000; n <= k; (sum < 2000000) ? ++n : --k) {
-        sum = (n*(n+1) * k*(k+1)) >> 2;
+
+    int n = 1, k = 2000;
+    unsigned int n_sq = 2;
+    unsigned int k_sq = 2000*2001;
+    while (n <= k) {
+        sum = (n_sq * k_sq) >> 2;
         int diff = abs(sum - 2000000);
         if (diff < closest_diff) {
             closest_diff = diff;
             area = k*n;
         }
+        if (sum < 2000000) {
+            n_sq = (++n)*(n+1);
+        } else {
+            k_sq = (--k)*(k+1);
+        }
     }
-    printf("%d %d\n", area, closest_diff);
+    printf("Area: %d, Closest Diff: %d\n", area, closest_diff);
 }
