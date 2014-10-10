bc0a21f4e2f61a1d5d35bc93c69aee0b440a064b
Favian Contreras <fnc4@cornell.edu>
2014-10-10 02:59:30 -0700

    Fixed abs macro.

--- p_85.c
+++ p_85.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define abs(x) ((x) < 0 ? (-x) : (x))
+#define abs(x) ((x) < 0 ? (-(x)) : (x))
 
 /* Find area of grid whose number of embedded rectangles is closest to 2,000,000. */
 
@@ -9,7 +9,7 @@ int main() {
     int closest_diff = 5000000;
     for (int n = 1, k = 2000; n <= k; (sum < 2000000) ? ++n : --k) {
         sum = (n*(n+1) * k*(k+1)) >> 2;
-        int diff = abs((sum - 2000000));
+        int diff = abs(sum - 2000000);
         if (diff < closest_diff) {
             closest_diff = diff;
             area = k*n;
